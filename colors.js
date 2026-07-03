88f71dd2e5b7d7dbadd31f2064e2a609c34e36ae
Bhupinder Kumar <bhupinderk0511@gmail.com>
2026-07-03 15:48:27 +0530

    fixes

--- colors.js
+++ colors.js
@@ -5,4 +5,4 @@ console.log(colors.yellow('hello Colors Module'));
 console.log(colors.green('hello Colors Module'));
 console.log(colors.cyan('cyan Colors Module'));
 console.log(colors.bgMagenta('cyan Colors Module'));
-console.log(colors.rainbow('cyanColorsModule'));
+console.log(colors.rainbow('Rain Bow Color Module'));
