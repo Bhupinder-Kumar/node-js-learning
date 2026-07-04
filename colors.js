12a49c18e194dd498146901ed9b28e2dbdf19c1c
Bhupinder Kumar <bhupinderk0511@gmail.com>
2026-07-04 13:06:16 +0530

    intallation of nodemon server and multiple server creation and run

--- colors.js
+++ colors.js
@@ -6,3 +6,4 @@ console.log(colors.green('hello Colors Module'));
 console.log(colors.cyan('cyan Colors Module'));
 console.log(colors.bgMagenta('cyan Colors Module'));
 console.log(colors.rainbow('Rain Bow Color Module'));
+console.log(colors.blue('Blue Color Module'));
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const port = 4200;
+const port2 = 5200;
 
 const server = http.createServer((req, res)=>{
     res.write('<h1>hello new server created\n </h1>');
@@ -7,3 +8,11 @@ const server = http.createServer((req, res)=>{
 });
 
 server.listen(port);
+
+
+const server2 = http.createServer((req, res)=>{
+    res.write('<h1>hello Second server created\n </h1>');
+    res.end('<p>Hello World\n<p>');
+});
+
+server2.listen(port2);
